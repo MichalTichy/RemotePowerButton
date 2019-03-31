c09f04b240c7360f581988d5b8085796192994d9
Michal Tichý <michal.tichy@riganti.cz>
2019-03-31 19:14:22 +0200

    Added IO lib

---
+++ RemotePowerButton.IO/PowerButton.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using IctBaden.RasPi.IO;
+
+namespace RemotePowerButton.IO
+{
+    public class PowerButton
+    {
+        protected DigitalIo io;
+        protected Output MotherBoardPowerBtn;
+        protected Input HardwarePowerBtn;
+        
+        public PowerButton()
+        {
+            InitIO();
+        }
+
+        protected void InitIO()
+        {
+            io = new DigitalIo();
+            if (!io.Initialize())
+                throw new IOException("Cannot initialize IO");
+
+            MotherBoardPowerBtn = io.CreateOutput(Gpio.Gpio4);
+            HardwarePowerBtn = io.CreateInput(Gpio.Gpio17);
+        }
+
+        public void PressPowerBtn(int duration = 250)
+        {
+            lock (MotherBoardPowerBtn)
+            {
+                MotherBoardPowerBtn.Set(true);
+                Thread.Sleep(duration);
+                MotherBoardPowerBtn.Set(false);
+            }
+        }
+    }
+}
